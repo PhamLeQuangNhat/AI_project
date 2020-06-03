f7e8e48c8f72989aeef27dbfc017ab113c34d290
quangnhat <18520120@gm.uit.edu.vn>
2020-06-04 03:09:34 +0700

    loading

--- Project/dlgo/networks/cnn.py
+++ Project/dlgo/networks/cnn.py
@@ -4,52 +4,47 @@ from keras.layers.convolutional import ZeroPadding2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
-from keras import backend as K 
 
 class Model:
     @staticmethod
     def build(width, height, depth, classes):
         # initialize model
         model = Sequential()
-        inputShape = (height, width, depth)
-
-        # if we are using "channels first", update the input shape
-        if K.image_data_format() == "channels_first":
-            inputShape = (depth, height, width)
-
+        inputShape = (depth, height, width)
+      
         # first ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((3, 3), input_shape=inputShape))
-        model.add(Conv2D(64, (7, 7), padding='valid'))
+        model.add(ZeroPadding2D((3, 3), input_shape=inputShape, data_format='channels_first'))
+        model.add(Conv2D(64, (7, 7), padding='valid',data_format='channels_first'))
         model.add(Activation('relu'))
         
         # second ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((2, 2)))
-        model.add(Conv2D(64, (5, 5)))
+        model.add(ZeroPadding2D((2, 2),data_format='channels_first'))
+        model.add(Conv2D(64, (5, 5),data_format='channels_first'))
         model.add(Activation('relu'))
 
         # third ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((2, 2)))
-        model.add(Conv2D(64, (5, 5)))
+        model.add(ZeroPadding2D((2, 2),data_format='channels_first'))
+        model.add(Conv2D(64, (5, 5),data_format='channels_first'))
         model.add(Activation('relu'))
 
         # forth ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((2, 2)))
-        model.add(Conv2D(48, (5, 5)))
+        model.add(ZeroPadding2D((2, 2),data_format='channels_first'))
+        model.add(Conv2D(48, (5, 5),data_format='channels_first'))
         model.add(Activation('relu'))
         
         # fith ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((2, 2)))
-        model.add(Conv2D(48, (5, 5)))
+        model.add(ZeroPadding2D((2, 2),data_format='channels_first'))
+        model.add(Conv2D(48, (5, 5),data_format='channels_first'))
         model.add(Activation('relu'))
 
         # sixth ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((2, 2)))
-        model.add(Conv2D(32, (5, 5)))
+        model.add(ZeroPadding2D((2, 2),data_format='channels_first'))
+        model.add(Conv2D(32, (5, 5),data_format='channels_first'))
         model.add(Activation('relu'))
 
         # seventh ZeroPadding => CONV => RELU
-        model.add(ZeroPadding2D((2, 2)))
-        model.add(Conv2D(32, (5, 5)))
+        model.add(ZeroPadding2D((2, 2),data_format='channels_first'))
+        model.add(Conv2D(32, (5, 5),data_format='channels_first'))
         model.add(Activation('relu'))
 
         # first (and only) FC => RELU 
@@ -64,3 +59,4 @@ class Model:
         # return the constructed network architecture
         return model
     
+
--- Project/test.py
+++ Project/test.py
@@ -2,7 +2,6 @@ from dlgo.agent.predict import DeepLearningAgent, load_prediction_agent
 from dlgo.data.parallel_processor import GoDataProcessor
 from dlgo.encoders.sevenplane import SevenPlaneEncoder
 from keras.optimizers import SGD
-#from dlgo.httpfrontend import get_web_app
 from dlgo.networks import cnn
 import h5py
 
@@ -18,11 +17,12 @@ opt = SGD(lr=0.005)
 model = cnn.Model.build(go_board_rows, go_board_cols,encoder.num_planes,nb_classes)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X, y, batch_size=128, epochs=20, verbose=1)
+model.fit(X, y, batch_size=128, epochs=100, verbose=1)
 
 deep_learning_bot = DeepLearningAgent(model, encoder)
-with h5py.File('./agents/deep_bot.h5', 'w') as outf:
+with h5py.File('H5PY/deep_bot.h5', 'w') as outf:
     deep_learning_bot.serialize(outf)
+
 
 
 
