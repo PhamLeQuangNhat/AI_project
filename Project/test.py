3b2d45f4496699a8c3ef0412352f2cdb4434ea62
quangnhat <18520120@gm.uit.edu.vn>
2020-06-03 23:24:41 +0700

    Project

--- Project/test.py
+++ Project/test.py
@@ -1,3 +1,53 @@
+import h5py
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+from dlgo.agent.predict import DeepLearningAgent, load_prediction_agent
+from dlgo.datapreprocessing.parallel_processor import GoDataProcessor
+from dlgo.encoders.sevenplane import SevenPlaneEncoder
+#from dlgo.httpfrontend import get_web_app
+from dlgo.networks import cnn
+
+go_board_rows, go_board_cols = 19, 19
+nb_classes = go_board_rows * go_board_cols
+encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
+processor = GoDataProcessor(encoder=encoder.name())
+
+X, y = processor.load_go_data(num_samples=100)
+
+input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
+model = Sequential()
+network_layers = cnn.layers(input_shape)
+for layer in network_layers:
+    model.add(layer)
+model.add(Dense(nb_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, y, batch_size=128, epochs=20, verbose=1)
+
+deep_learning_bot = DeepLearningAgent(model, encoder)
+with h5py.File('./agent/deep_bot.h5', 'w') as outf:
+    deep_learning_bot.serialize(outf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from dlgo.data.parallel_processor import GoDataProcessor
 from dlgo.encoders.oneplane import OnePlaneEncoder
 from dlgo.networks import small
